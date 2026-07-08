02fc5437f1d4e591c9cb31c7721a2fc365b24ed5
PawelBuczek <pawel.buczek@email.com>
2026-07-09 00:03:49 +0200

    browser import fix

--- src/time.ts
+++ src/time.ts
@@ -14,37 +14,38 @@ export function setSkew(callback: () => void) {
     return
   }
 
-  const OBR = require("@owlbear-rodeo/sdk").default
+  import("@owlbear-rodeo/sdk").then(({ default: OBR }) => {
 
-  OBR.onReady(() => {
-    const timeSyncUrl = getTimeSyncUrl(window.location.href)
-    console.log("fetching time from", timeSyncUrl)
-    fetch(timeSyncUrl, { cache: "no-store" }).then(r => {
-      // get the now time as soon as possible after the fetch
-      const now = new Date()
+    OBR.onReady(() => {
+      const timeSyncUrl = getTimeSyncUrl(window.location.href)
+      console.log("fetching time from", timeSyncUrl)
+      fetch(timeSyncUrl, { cache: "no-store" }).then(r => {
+        // get the now time as soon as possible after the fetch
+        const now = new Date()
 
-      // validate the header exists
-      const dateHeader = r.headers.get("date")
-      if (dateHeader === null) {
-        throw new ObrError("Date header failure: Header is null")
-      }
+        // validate the header exists
+        const dateHeader = r.headers.get("date")
+        if (dateHeader === null) {
+          throw new ObrError("Date header failure: Header is null")
+        }
 
-      // validate the header is a valid time
-      const serverTime = new Date(dateHeader)
-      if (isNaN(serverTime.getTime())) {
-        throw new ObrError(
-          `Date header failure: Unable to convert into Date: ${dateHeader}`,
+        // validate the header is a valid time
+        const serverTime = new Date(dateHeader)
+        if (isNaN(serverTime.getTime())) {
+          throw new ObrError(
+            `Date header failure: Unable to convert into Date: ${dateHeader}`,
+          )
+        }
+
+        // set the skew
+        skew = serverTime.getTime() - now.getTime()
+        console.log(
+          `server time: ${serverTime}\nlocal time:  ${now}\nskew: ${skew}`,
         )
-      }
 
-      // set the skew
-      skew = serverTime.getTime() - now.getTime()
-      console.log(
-        `server time: ${serverTime}\nlocal time:  ${now}\nskew: ${skew}`,
-      )
-
-      // call the call back
-      callback()
+        // call the call back
+        callback()
+      })
     })
   })
 }
