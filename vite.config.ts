a5c9ab38ba922438916963e1182e98d357a0a8b5
PawelBuczek <pawel.buczek@email.com>
2026-07-12 00:05:23 +0200

    cleaned vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 700,
-  },
   test: {
     environment: "jsdom",
     globals: true,
